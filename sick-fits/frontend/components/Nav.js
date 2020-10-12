f7379f6d3d4561711f787d6ee4ef757feb3c104c
Wendy Dherin <wendydherin@gmail.com>
2020-10-12 19:52:14 +0200

    Add Header, Meta, Nav, and Page components, custom app and doc, Home and Sell pages, styles and nProgress bar.

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,29 @@
+import { setMilliseconds } from 'date-fns';
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
+const Nav = () => {
+  return (
+    <NavStyles>
+      <Link href="/items">
+        <a>Items</a>
+      </Link>
+      <Link href="/sell">
+        <a>Sell</a>
+      </Link>
+      <Link href="/signup">
+        <a>Sign Up</a>
+      </Link>
+      <Link href="/orders">
+        <a>Orders</a>
+      </Link>
+      <Link href="/me">
+        <a>Account</a>
+      </Link>
+    </NavStyles>
+
+    
+  );
+};
+
+export default Nav;
